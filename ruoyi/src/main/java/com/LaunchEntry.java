74ef4c67c773450074a447498c485133eb87cf4e
shenzulun <shenzulun@gmail.com>
2020-05-15 15:23:01 +0800

    个性化

--- ruoyi/src/main/java/com/LaunchEntry.java
+++ ruoyi/src/main/java/com/LaunchEntry.java
@@ -1,21 +1,26 @@
-package com.ruoyi;
+/**
+ * File Name: LaunchEntry.java
+ * Date: 2020-05-14 15:49:34
+ */
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
- * 启动程序
- * 
- * @author ruoyi
+ * Description: 
+ * @author shenzulun
+ * @date 2020-05-14
+ * @version 1.0
  */
-//@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class RuoYiApplication
-{
-    public static void main(String[] args)
-    {
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class LaunchEntry {
+	
+	
+	public static void main(String[] args){
         System.setProperty("spring.devtools.restart.enabled", "false");
-        SpringApplication.run(RuoYiApplication.class, args);
+        SpringApplication.run(LaunchEntry.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
                 " .-------.       ____     __        \n" +
                 " |  _ _   \\      \\   \\   /  /    \n" +
