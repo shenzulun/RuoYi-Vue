03c6f6b05676f69aa85ba3f57c035d42e1dbed22
shenzulun <shenzulun@gmail.com>
2020-05-26 16:05:30 +0800

    配置调整
    Signed-off-by: shenzulun <shenzulun@gmail.com>

--- ruoyi/src/main/java/com/LaunchEntry.java
+++ ruoyi/src/main/java/com/LaunchEntry.java
@@ -4,32 +4,26 @@
  */
 package com;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
- * Description: 
+ * Description: 启动入口
  * @author shenzulun
  * @date 2020-05-14
  * @version 1.0
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class LaunchEntry {
-	
+    private static final Logger log = LoggerFactory.getLogger(LaunchEntry.class);
+
 	
 	public static void main(String[] args){
         System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(LaunchEntry.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        log.info("启动成功...");
     }
 }
